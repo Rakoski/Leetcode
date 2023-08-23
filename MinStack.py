60bd863b639bc68869b9a9d32491db7c0314669e
Rakoski <mastrakoski@gmail.com>
2023-08-23 10:56:18 -0300

    Added MinStack.py

---
+++ MinStack.py
@@ -0,0 +1,56 @@
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+#
+# Implement the MinStack class:
+#
+#     MinStack() initializes the stack object.
+#     void push(int val) pushes the element val onto the stack.
+#     void pop() removes the element on the top of the stack.
+#     int top() gets the top element of the stack.
+#     int getMin() retrieves the minimum element in the stack.
+#
+# You must implement a solution with O(1) time complexity for each function.
+
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []  # Initialize an empty list to store stack elements
+        self.min_stack = []  # Initialize an empty list to store minimum elements
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+
+        # Updating the min_stack with the new minimum value
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.stack:
+            # Check if the element being popped is the current minimum
+            if self.stack[-1] == self.min_stack[-1]:
+                self.min_stack.pop()
+            self.stack.pop()
+            # I tried doing this in the getMin function and it was way too complex.
+            # Since it was going to get a O(n) time complexity, while doing it here
+            # just has a O(1) time complexity
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.min_stack:
+            return self.min_stack[-1]
+
+
