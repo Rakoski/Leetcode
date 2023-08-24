285b528ee67469e33fa51260ae75c3c3f475c4a3
Rakoski <mastrakoski@gmail.com>
2023-08-24 13:21:35 -0300

    ReverseLinkedList.py

---
+++ ReverseLinkedList.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# Import Optional (I have never ever used Optional in Python lol)
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+
+        while cur:
+            arr = cur.next
+            cur.next = prev
+            prev = cur
+            cur = arr
+        return prev
