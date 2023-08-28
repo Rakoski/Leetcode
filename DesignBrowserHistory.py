caf9abb392228a5d0d2e87aec6ffad074fc9487c
Rakoski <mastrakoski@gmail.com>
2023-08-28 08:15:53 -0300

    Added some comentaries for better understanding

--- DesignBrowserHistory.py
+++ DesignBrowserHistory.py
@@ -16,15 +16,15 @@ class BrowserHistory:
         self.cur = self.cur.next
 
     def back(self, steps: int) -> str:
-        # if the number of steps becomes bigger than x, return up to the x value
+        # if the number of steps becomes bigger than x, go back the most you can, so that there's a limit
         while self.cur.prev and steps > 0:
-            self.cur = self.cur.prev
-            steps -= 1
-        return self.cur.val
+            self.cur = self.cur.prev  # the current page is now the previous page of the current page
+            steps -= 1  # diminish 1 step
+        return self.cur.val  # once the steps end, return the page you're at
 
-    def forward(self, steps: int) -> str:
+    def forward(self, steps: int) -> str:  # same thing, but forward this time
         while self.cur.next and steps > 0:
-            self.cur = self.cur.next
+            self.cur = self.cur.next  # instead of self.cur.prev it's self.cur.next
             steps -= 1
         return self.cur.val
 
