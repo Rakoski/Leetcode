39863baa4d0c4bd600f4da1e6bfe3b168e1999ea
Rakoski <mastrakoski@gmail.com>
2023-09-17 13:26:58 -0300

    Sort an array, merge sort

--- sort_an_array.py
+++ sort_an_array.py
@@ -17,5 +17,41 @@ class Solution:
         return nums
 
 
+# I will, then, use a merge-sort approach:
+
+def sortArray(nums: list[int]) -> list[int]:
+    def merge(arr, L, M, R):
+        left, right = arr[L:M+1], arr[M+1:R+1]
+        i, j, k = L, 0, 0
+
+        while j < len(left) and k < len(right):
+            if left[j] <= right[k]:
+                arr[i] = left[j]
+                j += 1
+            else:
+                arr[i] = right[k]
+                k += 1
+            i += 1
+        while j < len(left):
+            nums[i] = left[j]
+            j += 1
+            i += 1
+        while k < len(right):
+            nums[i] = right[k]
+            k += 1
+            i += 1
+
+    def merge_sort(arr, LEFT, RIGHT):
+        if LEFT == RIGHT:
+            return arr
+
+        MID = (LEFT + RIGHT) // 2
+        merge_sort(arr, LEFT, MID)
+        merge_sort(arr, MID + 1, RIGHT)
+        merge(arr, LEFT, MID, RIGHT)
+
+        return arr
+
+    return merge_sort(nums, 0, len(nums) - 1)
 
 
