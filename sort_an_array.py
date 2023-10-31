e14fbce242671dd490223d1554c3148cd5196225
Rakoski <mastrakoski@gmail.com>
2023-10-31 12:57:47 -0300

    quicksort, sort an array

--- sort_an_array.py
+++ sort_an_array.py
@@ -95,4 +95,31 @@ def sortArray(nums: list[int]) -> list[int]:
 
     return merge_sort(nums, 0, len(nums) - 1)
 
+# Now, I will implement it in quicksort
+
+def quickSort(alist):
+   quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+   if first<last:
+
+       splitpoint = partition(alist,first,last)
+
+       quickSortHelper(alist,first,splitpoint-1)
+       quickSortHelper(alist,splitpoint+1,last)
+
+
+def partition(alist,first,last):
+   pivotvalue = alist[first]
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+
 
